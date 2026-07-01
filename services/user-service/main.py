6f8d1dd32e8728161ac024af6b9004239ed0b066
siva1403 <51404790+siva1403@users.noreply.github.com>
2026-07-01 15:43:24 +0530

    Update main.py

--- services/user-service/main.py
+++ services/user-service/main.py
@@ -1,13 +1,38 @@
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# users = [{"siva" :"krishna"}]
+
+# @app.post("/users")
+# def create_user(user: dict):
+#     users.append(user)
+#     return {"message": "User created", "user": user}
+
+# @app.get("/users")
+# def get_users():
+#     return users
+
+
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-users = [{"siva" :"krishna"}]
+class User(BaseModel):
+    name: str
+    age: int
+
+users = []
 
 @app.post("/users")
-def create_user(user: dict):
-    users.append(user)
-    return {"message": "User created", "user": user}
+def create_user(user: User):
+    users.append(user.model_dump())
+    return {
+        "message": "User created",
+        "user": user
+    }
 
 @app.get("/users")
 def get_users():
