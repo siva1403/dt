311904b1d166006145584ba758b1685975545187
siva1403 <51404790+siva1403@users.noreply.github.com>
2026-07-01 15:52:48 +0530

    Refactor user service endpoints and add User model
    Refactored user creation and retrieval endpoints. Added User model for better data handling.

--- services/user-service/main.py
+++ services/user-service/main.py
@@ -1,39 +1,47 @@
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# users = [{"siva" :"krishna"}]
-
-# @app.post("/users")
-# def create_user(user: dict):
-#     users.append(user)
-#     return {"message": "User created", "user": user}
-
-# @app.get("/users")
-# def get_users():
-#     return users
-
-
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class User(BaseModel):
-    name: str
-    age: int
+users = [{"siva" :"krishna"}]
 
-users = []
-
+user = User(name="Krishna", age=24)
 @app.post("/users")
 def create_user(user: User):
-    users.append(user.model_dump())
-    return {
-        "message": "User created",
-        "user": user
-    }
+    print(user)
+    return {"user": user}
+
+
+@app.post("/users")
+def create_user(user: dict):
+    users.append(user)
+    return {"message": "User created", "user": user}
 
 @app.get("/users")
 def get_users():
     return users
+
+
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+# app = FastAPI()
+
+# class User(BaseModel):
+#     name: str
+#     age: int
+
+# users = []
+
+# @app.post("/users")
+# def create_user(user: User):
+#     users.append(user.model_dump())
+#     users.append()
+#     return {
+#         "message": "User created",
+#         "user": user
+#     }
+
+# @app.get("/users")
+# def get_users():
+#     return users
