1cf419bb75144018db22b6db18add17a4d7e62d7
siva1403 <51404790+siva1403@users.noreply.github.com>
2026-07-01 20:50:53 +0530

    Update main.py

--- services/user-service/main.py
+++ services/user-service/main.py
@@ -1,66 +1,3 @@
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# users = [{"siva" :"krishna"}]
-
-# user = User(name="Krishna", age=24)
-# @app.post("/users")
-# def create_user(user: User):
-#     print(user)
-#     return {"user": user}
-
-
-# @app.post("/users")
-# def create_user(user: dict):
-#     users.append(user)
-#     return {"message": "User created", "user": user}
-
-# @app.get("/users")
-# def get_users():
-#     return users
-
-
-
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-
-# app = FastAPI()
-
-# class User(BaseModel):
-#     name: str
-#     age: int
-
-# users = []
-
-# @app.post("/users")
-# def create_user(user: User):
-#     users.append(user.model_dump())
-#     users.append()
-#     return {
-#         "message": "User created",
-#         "user": user
-#     }
-
-# @app.get("/users")
-# def get_users():
-#     return users
-
-
-
-
-
-
-
-from pydantic import BaseModel
-
-class User(BaseModel):
-    name: str
-    age: int
-
-user = User(name="Krishna", age=24)
-print(user)
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -74,7 +11,7 @@ users = []
 
 @app.post("/users")
 def create_user(user: User):
-    users.append(user.model_dump())
+    users.append(user.dict())
     return {
         "message": "User created",
         "user": user
